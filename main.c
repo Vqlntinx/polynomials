aa173b82034bffb977653ef55a31252ed58b5e86
Vqlntinx <161123515+Vqlntinx@users.noreply.github.com>
2024-11-04 10:55:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ int main() {
     Term *poly1 = NULL;
     Term *poly2 = NULL;
 
-    addTerm(&poly1, 5, 2);
-    addTerm(&poly1, 3, 3);
-    addTerm(&poly1, 2, 0);
-    addTerm(&poly2, 4, 1);
-    addTerm(&poly2, 3, 3);
-    addTerm(&poly2, 7, 0);
+    appendTerm(&poly1, 5, 2);
+    appendTerm(&poly1, 3, 3);
+    appendTerm(&poly1, 2, 0);
+    appendTerm(&poly2, 4, 1);
+    appendTerm(&poly2, 3, 3);
+    appendTerm(&poly2, 7, 0);
 
     printf("Polynomial 1: ");
     printPoly(poly1);
